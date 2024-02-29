31d8964107fafadb8bbc2e01f4267f46d65dc8e6
meister42 <hgh585@utulsa.edu>
2024-02-29 14:52:19 -0600

    problem 2

--- Problem#2/Palindrome.py
+++ Problem#2/Palindrome.py
@@ -1,5 +1,17 @@
 def check_palindrome(user_input):
- #type your code here
+    reverse = ""
+    for i in user_input: 
+        reverse = i + reverse
+
+    if reverse == user_input:
+        return True
+    else:
+        return False
+    
 if __name__ == "__main__":
     user_input = input()
     check_palindrome(user_input)
+    if check_palindrome(user_input) == True:
+        print("palindrome: " + str(user_input))
+    else:
+        print("not a palindrome: " + str(user_input))
