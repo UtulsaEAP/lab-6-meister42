e8497b12cbf2dd83cfcdc31342f04d01ec86c421
meister42 <hgh585@utulsa.edu>
2024-02-29 14:35:00 -0600

    problem 1

--- Problem #1/madlip.py
+++ Problem #1/madlip.py
@@ -1,8 +1,26 @@
+"""
+Name: Henry Holman
+Lab Time: Thursday 2 pm
+
+"""
+
 def food_input():
     user_input = input()
-    tokens = user_input.split()
-    #type you while  loop here
+    tokens = user_input.split(" ")
+    tokendictionary = {}
+    while tokens[0] != "quit":
+        tokendictionary[tokens[0]] = tokens[1]
+        user_input = input()
+        tokens = user_input.split(" ")
+    
+    for i in tokendictionary:
+        print("Eating %s %s a day keeps you happy and healthy." % (tokendictionary[i], i))
+    
+    
+    
 
+    
+    
     
 
 if __name__ == "__main__":
