3ca0836c7f52dc83ffb5e40a0494dc8030a3dcc5
meister42 <hgh585@utulsa.edu>
2024-03-02 10:48:20 -0600

    problem 7

--- Problem#7/carwash.py
+++ Problem#7/carwash.py
@@ -1,10 +1,14 @@
 def calculate_car_wash_price(service_choice1, service_choice2):
     services = {'Air freshener': 1, 'Rain repellent': 2, 'Tire shine': 2, 'Wax': 3, 'Vacuum': 5}
     base_wash = 10
-    total = 0
-   
-   #type your code here 
+    total = services[service_choice1] + services[service_choice2] + base_wash
 
+    print(f"    ZyCar Wash")
+    print(f"    Base Car Wash - $10")
+    print(f"    {service_choice1} - ${services[service_choice1]}")
+    print(f"    {service_choice2} - ${services[service_choice2]}")
+    print(f"""-----
+    Total price: ${total}""")
     
 if __name__ == '__main__':
     # Get user input for service choices
